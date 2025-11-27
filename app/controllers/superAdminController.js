45c62812163fec8d5d0c2983b99143f08d6ca07f
Basil Abo Saleem <basilsaleem11@gmail.com>
2025-11-27 10:44:52 +0200

    feat(superAdminController): add controller to render company details page
    - Fetches company and associated admins via CompanyService
    - Renders 'companyDetails' EJS view with all necessary data
    - Handles errors and displays 500 status on failure

--- app/controllers/superAdminController.js
+++ app/controllers/superAdminController.js
@@ -1,4 +1,6 @@
 const Company = require('../models/Company');
+const CompanyService = require('../services/superAdminServices');
+
 exports.renderPendingCompaniesPage = async (req, res) => {
     try {
         const companies = await Company.find({ status: 'pending' });
@@ -8,6 +10,23 @@ exports.renderPendingCompaniesPage = async (req, res) => {
         });
     } catch (error) {
         console.error(error);
+        res.status(500).send('Server Error');
+    }
+}
+
+exports.renderCompanyDetailsPage = async (req, res) => {
+    const companyId = req.params.id;
+    try {
+        const { company, admins, adminRelations  } = await CompanyService.getCompanyDetails(companyId);
+
+        res.render('dashboard/pages/superAdmin/companies/companyDetails', {
+            title: 'Company Details',
+            company,
+            admins,
+            adminRelations
+        });
+    } catch (error) {
+        console.error('Company Details Error:', error);
         res.status(500).send('Server Error');
     }
 }
