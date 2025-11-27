3fdc527bd443e3d25a04ab9247893718d55ea0f1
Basil Abo Saleem <basilsaleem11@gmail.com>
2025-11-27 10:41:48 +0200

    feat(superadmin): add route for viewing company details
    - Added GET /superadmin/companies/:id/view route
    - Linked route to renderCompanyDetailsPage controller
    - Route now allows Super Admin to access company details page

--- app/routes/superAdminRoutes.js
+++ app/routes/superAdminRoutes.js
@@ -5,5 +5,5 @@ const superAdminController = require('../controllers/superAdminController');
 const authorize = require('../middlewares/authorize');
 
 router.get('/superadmin/companies/pending-approval',authorize([]) , superAdminController.renderPendingCompaniesPage);
-
+router.get('/superadmin/companies/:id/view',authorize([]) , superAdminController.renderCompanyDetailsPage);
 module.exports = router; 
